7ed2155a3be3e6ef19edf4b859141b548be70fd7
drod75 <dr507498@gmail.com>
2024-09-15 15:02:18 -0400

    'login_added'

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirmed_password = PasswordField('Confirm your Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm your Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Confirm')
 
 class LoginForm(FlaskForm):
--- routes.py
+++ routes.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__, static_folder='static')
+app.config['SECRET_KEY'] = 'd23b5a76c202b3ab3c4d5bccf4b1d985'
 
 @app.route('/')
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form, title='Register')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@gmail.com' and form.password.data == '123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form, title='Home')
 
 @app.route('/data-cleaning')
 def data_cleaning():
-    return render_template('cleaning.html')
+    return render_template('cleaning.html', title='Cleaning')
 
 @app.route('/data-processing')
 def data_processing():
-    return render_template('processing.html')
+    return render_template('processing.html', title='Processing')
